16596b571bed570697fe108b43c23d3ef2f37ec0
hirai-git <123721152+hirai-git@users.noreply.github.com>
2023-02-01 22:44:39 +0900

    Update streamlit_app01.py

--- streamlit_app01.py
+++ streamlit_app01.py
@@ -14,13 +14,11 @@ def get_query(query_file_path):
 
 def run_query():
    with my_cnx.cursor() as cur:
-            query = get_query(query_file_path)
             cur.execute("select * from NPMDB_test.NQIUSER1.ATTRIBUTES where SECURITY_CODE='1301' and CALENDAR_DATE between 20220101 and 20221231 order by CALENDAR_DATE")
             return cur.fetchall()
             
 if __name__ == "__main__":
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    query_file_path = 'npmdbtest.sql'
     my_data_rows=run_query()
-    streamlit.dataframe(my_data_rows)
     my_cnx.close()
+    streamlit.dataframe(my_data_rows)
