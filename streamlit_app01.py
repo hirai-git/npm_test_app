18beb2f1360b8f9ed413a4ceef7a277c5572db51
hirai-git <123721152+hirai-git@users.noreply.github.com>
2023-02-01 22:01:13 +0900

    Update streamlit_app01.py

--- streamlit_app01.py
+++ streamlit_app01.py
@@ -2,5 +2,25 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-streamlit hello
 
+from urllib.error import URLError
+#import snowflake.connector
+streamlit.header("NPMdata")
+
+def get_query(query_file_path):
+    with open(query_file_path, 'r', encoding='utf-8') as f:
+        query = f.read()
+    return query
+
+def run_query(query_file_path):
+   with my_cnx.cursor() as cur:
+            query = get_query(query_file_path)
+            cur.execute(query)
+            return cur.fetchall()
+            
+if __name__ == "__main__":
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    query_file_path = 'npmdbtest.sql'
+    my_data_rows=run_query(query_file_path)
+    streamlit.table(my_data_rows)
+    my_cnx.close()
