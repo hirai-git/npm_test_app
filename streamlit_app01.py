b4ecf1f661fbb4bd4359baab9f8ba2d862c4a931
hirai-git <123721152+hirai-git@users.noreply.github.com>
2023-02-01 22:10:55 +0900

    Update streamlit_app01.py

--- streamlit_app01.py
+++ streamlit_app01.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     query_file_path = 'npmdbtest.sql'
     my_data_rows=run_query(query_file_path)
-    streamlit.table(my_data_rows)
+    streamlit.DataFrame(my_data_rows,columns=["CALENDAR_DATE","CODE","KANJI_NAME","PRICE"])
     my_cnx.close()
