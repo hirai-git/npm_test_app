7bca83e809e5c8abcbfbde5c9d2b0880e7559fa7
hirai-git <123721152+hirai-git@users.noreply.github.com>
2023-02-01 21:40:01 +0900

    Update streamlit_app01.py

--- streamlit_app01.py
+++ streamlit_app01.py
@@ -3,6 +3,28 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
+#import snowflake.connector
+streamlit.header("NPM個別銘柄情報")
 
-streamlit hello
+def get_query(query_file_path):
+    with open(query_file_path, 'r', encoding='utf-8') as f:
+        query = f.read()
+    return query
+
+def run_query(query_file_path):
+   with my_cnx.cursor() as cur:
+            query = get_query(query_file_path)
+            cur.execute(query)
+            return cur.fetchall()
+            
+if __name__ == "__main__":
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    query_file_path = 'npmdbtest.sql'
+    my_data_rows=run_query(query_file_path)
+    streamlit.write(my_data_rows)
+    my_cnx.close()
+      
+
+
+
 
